7cf06317cef9169debb039187badcdfabcab576a
zeromake <a390720046@gmail.com>
2019-03-20 18:16:28 +0800

    feat: mocha -> jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "commonjs": true,
         "es6": true,
         "node": true,
-        "mocha": true,
+        "jest": true,
     },
     "extends": "eslint:recommended",
     "globals": {
