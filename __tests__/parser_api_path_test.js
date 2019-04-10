4bfaddcfa62794efcb3bd4a442921509c42073d5
zeromake <a390720046@gmail.com>
2019-04-10 21:57:55 +0800

    feat: add path test

---
+++ __tests__/parser_api_path_test.js
@@ -0,0 +1,101 @@
+/*jshint unused:false*/
+
+/**
+ * Test: Parser apiPath
+ */
+
+// node modules
+
+// lib modules
+var parser = require('../lib/parsers/api_path');
+
+describe('Parser: apiPath', () => {
+
+    // TODO: Add 1.000 more possible cases ;-)
+    var testCases = [
+        {
+            title: 'Simple fieldname only',
+            content: 'simple',
+            expected: {
+                group: 'Path',
+                type: undefined,
+                size: undefined,
+                allowedValues: undefined,
+                optional: false,
+                field: 'simple',
+                defaultValue: undefined,
+                description: ''
+            }
+        },
+        {
+            title: 'Type, Fieldname, Description',
+            content: '{String} name The users name.',
+            expected: {
+                group: 'Path',
+                type: 'String',
+                size: undefined,
+                allowedValues: undefined,
+                optional: false,
+                field: 'name',
+                defaultValue: undefined,
+                description: 'The users name.'
+            }
+        },
+        {
+            title: 'All options, with optional defaultValue',
+            content: ' ( MyGroup ) { \\Object\\String.uni-code_char[] { 1..10 } = \'abc\', \'def\' }  ' +
+                     '[ \\MyClass\\field.user_first-name = \'John Doe\' ] Some description.',
+            expected: {
+                group: 'MyGroup',
+                type: '\\Object\\String.uni-code_char[]',
+                size: '1..10',
+                allowedValues: [ '\'abc\'', '\'def\'' ],
+                optional: true,
+                field: '\\MyClass\\field.user_first-name',
+                defaultValue: 'John Doe',
+                description: 'Some description.'
+            }
+        },
+        {
+            title: 'All options, without optional-marker',
+            content: ' ( MyGroup ) { \\Object\\String.uni-code_char[] { 1..10 } = \'abc\', \'def\' }  ' +
+                     '\\MyClass\\field.user_first-name = \'John Doe\' Some description.',
+            expected: {
+                group: 'MyGroup',
+                type: '\\Object\\String.uni-code_char[]',
+                size: '1..10',
+                allowedValues: [ '\'abc\'', '\'def\'' ],
+                optional: false,
+                field: '\\MyClass\\field.user_first-name',
+                defaultValue: 'John Doe',
+                description: 'Some description.'
+            }
+        },
+        {
+            title: 'All options, without optional-marker, without default value quotes',
+            content: ' ( MyGroup ) { \\Object\\String.uni-code_char[] { 1..10 } = \'abc\', \'def\' }  ' +
+                     '\\MyClass\\field.user_first-name = John_Doe Some description.',
+            expected: {
+                group: 'MyGroup',
+                type: '\\Object\\String.uni-code_char[]',
+                size: '1..10',
+                allowedValues: [ '\'abc\'', '\'def\'' ],
+                optional: false,
+                field: '\\MyClass\\field.user_first-name',
+                defaultValue: 'John_Doe',
+                description: 'Some description.'
+            }
+        },
+    ];
+
+    // create
+    test('case 1: should pass all regexp test cases', done => {
+        testCases.forEach(function(testCase) {
+            var parsed = parser.parse(testCase.content);
+            expect(parsed !== null).toBe(true);
+            expect(parsed).toEqual(testCase.expected);
+        });
+        done();
+    });
+
+});
